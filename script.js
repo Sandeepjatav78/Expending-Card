5e2d7dfe4e0eb38da74663f0c45a41789df53d17
Sandeep  kumar <120194745+Sandeepjatav78@users.noreply.github.com>
2024-05-04 22:15:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,164 @@
+const first = document.querySelector(".image1");
+const second = document.querySelector(".image2");
+const third = document.querySelector(".image3");
+const forth = document.querySelector(".image4");
+const fifth = document.querySelector(".image5");
+const body = document.querySelector("body");
+
+function hoverOnBody() {
+    first.style.width = '7%'
+    second.style.width = '7%'
+    third.style.width = '7%'
+    fifth.style.width = '7%'
+    forth.style.width = '7%'
+}
+
+function hover1() {
+    if (window.innerWidth > 660) {
+        first.style.width = '100%'
+        second.style.width = '7%'
+        third.style.width = '7%'
+        forth.style.width = '7%'
+        fifth.style.width = '7%'
+
+        first.style.height = '100%'
+        second.style.height = '100%'
+        third.style.height = '100%'
+        forth.style.height = '100%'
+        fifth.style.height = '100%'
+    } else {
+        first.style.width = '100%'
+        second.style.width = '100%'
+        third.style.width = '100%'
+        forth.style.width = '100%'
+        fifth.style.width = '100%'
+
+        first.style.height = '100%'
+        second.style.height = '20%'
+        third.style.height = '20%'
+        forth.style.height = '20%'
+        fifth.style.height = '20%'
+    }
+}
+
+
+function hover2() {
+    if (window.innerWidth > 660) {
+        first.style.width = '7%'
+        second.style.width = '100%'
+        third.style.width = '7%'
+        forth.style.width = '7%'
+        fifth.style.width = '7%'
+
+        first.style.height = '100%'
+        second.style.height = '100%'
+        third.style.height = '100%'
+        forth.style.height = '100%'
+        fifth.style.height = '100%'
+    }
+    else {
+        first.style.width = '100%'
+        second.style.width = '100%'
+        third.style.width = '100%'
+        forth.style.width = '100%'
+        fifth.style.width = '100%'
+
+        first.style.height = '20%'
+        second.style.height = '100%'
+        third.style.height = '20%'
+        forth.style.height = '20%'
+        fifth.style.height = '20%'
+    }
+}
+
+function hover3() {
+    if (window.innerWidth > 660) {
+        first.style.width = '7%'
+        second.style.width = '7%'
+        third.style.width = '100%'
+        forth.style.width = '7%'
+        fifth.style.width = '7%'
+
+        first.style.height = '100%'
+        second.style.height = '100%'
+        third.style.height = '100%'
+        forth.style.height = '100%'
+        fifth.style.height = '100%'
+    }
+    else {
+        first.style.width = '100%'
+        second.style.width = '100%'
+        third.style.width = '100%'
+        forth.style.width = '100%'
+        fifth.style.width = '100%'
+
+        first.style.height = '20%'
+        second.style.height = '20%'
+        third.style.height = '100%'
+        forth.style.height = '20%'
+        fifth.style.height = '20%'
+    }
+}
+
+function hover4() {
+    if (window.innerWidth > 660) {
+        first.style.width = '7%'
+        second.style.width = '7%'
+        third.style.width = '7%'
+        forth.style.width = '100%'
+        fifth.style.width = '7%'
+
+        first.style.height = '100%'
+        second.style.height = '100%'
+        third.style.height = '100%'
+        forth.style.height = '100%'
+        fifth.style.height = '100%'
+    }
+    else {
+        first.style.width = '100%'
+        second.style.width = '100%'
+        third.style.width = '100%'
+        forth.style.width = '100%'
+        fifth.style.width = '100%'
+
+
+        first.style.height = '20%'
+        second.style.height = '20%'
+        third.style.height = '20%'
+        forth.style.height = '100%'
+        fifth.style.height = '20%'
+    }
+}
+function hover5() {
+    if (window.innerWidth > 660) {
+        first.style.width = '7%'
+        second.style.width = '7%'
+        third.style.width = '7%'
+        forth.style.width = '7%'
+        fifth.style.width = '100%'
+
+        first.style.height = '100%'
+        second.style.height = '100%'
+        third.style.height = '100%'
+        forth.style.height = '100%'
+        fifth.style.height = '100%'
+    }
+    else {
+        first.style.width = '100%'
+        second.style.width = '100%'
+        third.style.width = '100%'
+        forth.style.width = '100%'
+        fifth.style.width = '100%'
+
+        first.style.height = '20%'
+        second.style.height = '20%'
+        third.style.height = '20%'
+        forth.style.height = '20%'
+        fifth.style.height = '100%'
+
+
+    }
+}
+
+
+
